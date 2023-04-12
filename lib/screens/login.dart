249510f815927b9186c397b0586be5d10516971c
nishantnz <nishant.z@somaiya.edu>
2023-04-12 20:06:35 +0530

    made google login screen and added dependency

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,4 @@
+import 'package:docs_clone/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -8,7 +9,21 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Center(
+        child: ElevatedButton.icon(
+          style: ButtonStyle(
+            minimumSize: MaterialStateProperty.all(const Size(150, 50)),
+            backgroundColor: MaterialStateProperty.all(WhiteColor),
+            foregroundColor: MaterialStateProperty.all(BlackColor),
+          ),
+          onPressed: () {},
+          icon: Image.asset(
+            "assets/images/g_logo.png",
+            height: 20,
+          ),
+          label: Text("Sign in with Google"),
+        ),
+      ),
     );
   }
 }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const BlackColor = Colors.black;
+const WhiteColor = Colors.white;
