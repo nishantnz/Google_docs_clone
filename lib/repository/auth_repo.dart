ebcbbb9d5b9f6622c36bca6f3a5f4912ed09253a
nishantnz <nishant.z@somaiya.edu>
2023-04-14 16:34:48 +0530

    creating google sign in auth with provider

---
+++ lib/repository/auth_repo.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  return AuthRepository(GoogleSignIn());
+});
+
+class AuthRepository {
+  final GoogleSignIn _googleSignIn;
+
+  AuthRepository(this._googleSignIn);
+
+  signInWithGoogle() async {
+    try {
+      final user = await _googleSignIn.signIn();
+      if (user != null) {
+        print(user.email);
+        print(user.displayName);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -2,12 +2,19 @@ import 'package:docs_clone/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends StatelessWidget {
+import '../repository/auth_repo.dart';
+
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authRepositoryProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
@@ -16,7 +23,9 @@ class LoginScreen extends StatelessWidget {
             backgroundColor: MaterialStateProperty.all(WhiteColor),
             foregroundColor: MaterialStateProperty.all(BlackColor),
           ),
-          onPressed: () {},
+          onPressed: () {
+            signInWithGoogle(ref);
+          },
           icon: Image.asset(
             "assets/images/g_logo.png",
             height: 20,
